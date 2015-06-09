fcbb737004c1e31601b6c8636627ff8ea2e53240
sumankum <sumankum@INsumankum.local>
2015-06-09 17:35:55 +0530

    Added code in Main class to run the tax problem for 1 product

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,20 @@
 package com.company;
 
 public class Main {
+    ConsoleInput input;
+    ConsoleOutput output;
+    Tax tax;
 
     public void start() {
-
+        input = new ConsoleInput();
+        output = new ConsoleOutput();
+        tax = new Tax(input.takeUserInputForMainMenu());
+        output.display(tax);
     }
+
     public static void main(String[] args) {
+        Main m = new Main();
+        m.start();
 
     }
 }
