b2e12944b048c205aa87240bdd1d6eae2c0512ae
sumankum <sumankum@INsumankum.local>
2015-06-09 15:35:08 +0530

    Added food class and the price was checked when it was imported

--- test/FoodTest.java
+++ test/FoodTest.java
@@ -3,15 +3,26 @@ import org.junit.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.*;
+import static org.junit.Assert.assertEquals;
 
 public class FoodTest {
     @Test
     public void shouldReturnSamePriceWhenItsGivenNotImported() {
-        Food food = new Food("Chocolate",0.85,false);
+        Food food = new Food("Chocolate", 0.85, false);
 
         double actualPrice = food.priceOnApplyingTax();
         double expectedPrice = 0.85;
 
-        assertThat(actualPrice,is(expectedPrice));
+        assertThat(actualPrice, is(expectedPrice));
+    }
+
+    @Test
+    public void shouldReturnDifferentPriceWhenItsGivenThatItIsImported() {
+        Food food = new Food("Chocolate", 11.25, true);
+
+        double actualPrice = food.priceOnApplyingTax();
+        double expectedPrice = 11.85;
+
+        assertEquals(actualPrice, expectedPrice, 0.05d);
     }
 }
