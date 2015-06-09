2d78beb39fd9605abf4ebbc93dd914eca4e46b56
sumankum <sumankum@INsumankum.local>
2015-06-09 16:00:07 +0530

    Added other commodities class and checked the value when it was imported

--- test/OtherCommoditiesTest.java
+++ test/OtherCommoditiesTest.java
@@ -5,8 +5,8 @@ import static org.junit.Assert.assertEquals;
 
 public class OtherCommoditiesTest {
     @Test
-    public void shouldReturnSamePriceWhenItsGivenNotImported() {
-        OtherCommodities other = new OtherCommodities("CD",14.99,false);
+    public void shouldReturnPriceWhenItsGivenNotImported() {
+        OtherCommodities other = new OtherCommodities("CD", 14.99, false);
 
         double actualPrice = other.priceOnApplyingTax();
         double expectedPrice = 16.49;
@@ -14,4 +14,13 @@ public class OtherCommoditiesTest {
         assertEquals(expectedPrice, actualPrice, 0.05d);
     }
 
+    @Test
+    public void shouldReturnMorePriceWhenItsGivenImported() {
+        OtherCommodities other = new OtherCommodities("CD", 47.50, true);
+
+        double actualPrice = other.priceOnApplyingTax();
+        double expectedPrice = 54.86;
+
+        assertEquals(expectedPrice, actualPrice, 0.05d);
+    }
 }
